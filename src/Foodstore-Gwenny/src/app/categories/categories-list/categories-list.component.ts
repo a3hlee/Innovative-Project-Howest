9b1dcec80cb776034735d4791e40d7a0d7c4f543
Ashley <ashley.senaeve@student.howest.be>
2024-01-03 20:40:40 +0100

    Cleanup and adding

--- src/Foodstore-Gwenny/src/app/categories/categories-list/categories-list.component.ts
+++ src/Foodstore-Gwenny/src/app/categories/categories-list/categories-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.css']
 })
-export class CategoriesListComponent {
+export class CategoriesListComponent implements OnInit{
+  categoriesArray: any[] = [];
 
+  constructor(private db: AngularFirestore) { }
+
+  ngOnInit(): void {
+    const collectionRef = this.db.collection('test');
+    const collectionInstance = collectionRef.valueChanges();
+    collectionInstance.subscribe(ss => this.categoriesArray = ss);
+    }
 }
